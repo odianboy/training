d904f646db708c77d27c5b2a847a5c4da0f162f9
Anton <odianboy@mail.ru>
2020-10-16 00:59:03 +0300

    Fixed

--- lesson_003/04_student.py
+++ lesson_003/04_student.py
@@ -10,16 +10,47 @@
 #   Студенту надо попросить ХХХ.ХХ рублей
 
 educational_grant, expenses = 10000, 12000
-month = 0   # Количество пройденных месяцев
+quantity_month = 0   # Количество пройденных месяцев
+month = 0
 grants = 0  # Стипендия
-costs = 0   # Ежемеечные расходы
+costs = 0  # Ежемеечные расходы
 
-while month != 10:  # До тех пор пока month не равен 10 (кол-во месяцев)
+while quantity_month != 10:  # До тех пор пока month не равен 10 (кол-во месяцев)
     grants += educational_grant     # Накопительный счет для стипендии
-    costs += expenses   # Первоночальный месяц
-    month += 1
-    if month == 1:
-        costs += expenses + (expenses * 0.03)  # Накопительный счет для расходов с повышением цен на 3%
+    if quantity_month == 0:
+        month = expenses
+        costs += month
+    elif quantity_month == 1:
+        month_2 = month + (month * 0.03)
+        costs += month_2
+    elif quantity_month == 2:
+        month_3 = month_2 + (month_2 * 0.03)
+        costs += month_3
+    elif quantity_month == 3:
+        month_4 = month_3 + (month_3 * 0.03)
+        costs += month_4
+    elif quantity_month == 4:
+        month_5 = month_4 + (month_4 * 0.03)
+        costs += month_5
+    elif quantity_month == 5:
+        month_6 = month_5 + (month_5 * 0.03)
+        costs += month_6
+    elif quantity_month == 6:
+        month_7 = month_6 + (month_6 * 0.03)
+        costs += month_7
+    elif quantity_month == 7:
+        month_8 = month_7 + (month_7 * 0.03)
+        costs += month_8
+    elif quantity_month == 8:
+        month_9 = month_8 + (month_8 * 0.03)
+        costs += month_9
+    elif quantity_month == 9:
+        month_10 = month_9 + month_9 * 0.03
+        costs += month_10
+
+    # print(month_10)# Накопительный счет для расходов с повышением цен на 3%
+
+    quantity_month += 1
 
 print('Студенту надо попросить', round(costs - grants, 2), "рублей")
 
