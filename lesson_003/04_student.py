07b983a8aed4ba68b3b80e69863fcd2d423ac015
Anton <odianboy@mail.ru>
2020-10-16 22:54:16 +0300

    Fixed by Most9

--- lesson_003/04_student.py
+++ lesson_003/04_student.py
@@ -10,47 +10,21 @@
 #   Студенту надо попросить ХХХ.ХХ рублей
 
 educational_grant, expenses = 10000, 12000
-quantity_month = 0   # Количество прожитых месяцев
-grants = 0  # Сумма ежемеечных стипендний
-costs = 0  # Сумма ежемеечных расходов
 
+month = 2
+max_month = 10
 
-while quantity_month != 10:  # До тех пор пока количество месяцев не равно 10
-    grants += educational_grant
-    if quantity_month == 0:
-        month = expenses
-        costs += month      # Накопительный счет для расходов с повышением цен на 3%
-    elif quantity_month == 1:
-        month_2 = month + (month * 0.03)
-        costs += month_2
-    elif quantity_month == 2:
-        month_3 = month_2 + (month_2 * 0.03)
-        costs += month_3
-    elif quantity_month == 3:
-        month_4 = month_3 + (month_3 * 0.03)
-        costs += month_4
-    elif quantity_month == 4:
-        month_5 = month_4 + (month_4 * 0.03)
-        costs += month_5
-    elif quantity_month == 5:
-        month_6 = month_5 + (month_5 * 0.03)
-        costs += month_6
-    elif quantity_month == 6:
-        month_7 = month_6 + (month_6 * 0.03)
-        costs += month_7
-    elif quantity_month == 7:
-        month_8 = month_7 + (month_7 * 0.03)
-        costs += month_8
-    elif quantity_month == 8:
-        month_9 = month_8 + (month_8 * 0.03)
-        costs += month_9
-    elif quantity_month == 9:
-        month_10 = month_9 + (month_9 * 0.03)
-        costs += month_10
+all_expenses = 12000
 
-    quantity_month += 1
+while max_month > month:
 
-print('Студенту надо попросить', round(costs - grants, 2), "рублей")
+    inflation = expenses * .03
+    expenses += inflation
+
+    all_expenses += expenses
+    month += 1
+
+print('Студенту надо попросить', round(all_expenses - educational_grant * 10), "рублей")
 
 
 
