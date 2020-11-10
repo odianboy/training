71d5eea22ba7b584f54fe3c1b7a240fdd447d828
Anton <odianboy@mail.ru>
2020-11-11 00:49:49 +0300

    trying to make streams

--- lesson_012/02_volatility_with_threads.py
+++ lesson_012/02_volatility_with_threads.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-
-
+import threading
+from collections import defaultdict
+import os
+import csv
 # Задача: вычислить 3 тикера с максимальной и 3 тикера с минимальной волатильностью в МНОГОПОТОЧНОМ стиле
 #
 # Бумаги с нулевой волатильностью вывести отдельно.
@@ -17,6 +19,43 @@
 #       ТИКЕР7, ТИКЕР8, ТИКЕР9, ТИКЕР10, ТИКЕР11, ТИКЕР12
 # Волатильности указывать в порядке убывания. Тикеры с нулевой волатильностью упорядочить по имени.
 #
-# TODO Внимание! это задание можно выполнять только после зачета lesson_012/01_volatility.py !!!
 
-# TODO тут ваш код в многопоточном стиле
+
+class Ticker(threading.Thread):
+
+    def __init__(self, dir_name, name_ticker, lock, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.dir_name = dir_name
+        self.lock_ticker = lock
+        self.name_ticker = name_ticker
+
+    def run(self):
+        self.result = {}
+        with open(f"{self.dir_name}/{self.name_ticker}") as ff:
+            total_list = []
+            spam_reader = list(csv.reader(ff))
+
+            for read in spam_reader[1:]:
+                self.price = float(read[2])
+                total_list.append(self.price)
+
+                average_price = sum(total_list) / len(total_list)
+                volatility = ((max(total_list)) - (min(total_list)) / average_price) * 100
+
+                self.result[self.name_ticker[7:11]] = round(volatility, 4)
+
+                total_list.clear()
+        print(self.result)
+
+
+if __name__ == '__main__':
+
+    dir_name = os.listdir('trades')
+    lock = threading.Lock
+    count = [Ticker(dir_name='trades', name_ticker=line_ticker, lock=lock) for line_ticker in dir_name]
+
+    for ticker in count:
+        ticker.start()
+
+    for ticker in count:
+       ticker.join()
