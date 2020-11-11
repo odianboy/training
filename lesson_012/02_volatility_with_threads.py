206b9745ec4071362ecc9c3f21581067d9e219e1
Anton <odianboy@mail.ru>
2020-11-11 20:11:51 +0300

    second try

--- lesson_012/02_volatility_with_threads.py
+++ lesson_012/02_volatility_with_threads.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import threading
-from collections import defaultdict
 import os
 import csv
+from threading import Thread
+from lesson_012.python_snippets.utils import time_track
+
 # Задача: вычислить 3 тикера с максимальной и 3 тикера с минимальной волатильностью в МНОГОПОТОЧНОМ стиле
 #
 # Бумаги с нулевой волатильностью вывести отдельно.
@@ -21,42 +23,57 @@ import csv
 #
 
 
-class Ticker(threading.Thread):
+class Ticker(Thread):
 
-    def __init__(self, dir_name, name_ticker, lock, *args, **kwargs):
+    def __init__(self, dir_name, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.dir_name = dir_name
-        self.lock_ticker = lock
-        self.name_ticker = name_ticker
 
     def run(self):
-        self.result = {}
-        with open(f"{self.dir_name}/{self.name_ticker}") as ff:
-            total_list = []
-            spam_reader = list(csv.reader(ff))
+        result = {}
+        null_result = {}
+        files = os.listdir(self.dir_name)
+        for file_name in files:
+            with open(f"{self.dir_name}/{file_name}") as ff:
+                total_list = []
+                spam_reader = list(csv.reader(ff))
+                ticker_name = file_name[7:11]
 
-            for read in spam_reader[1:]:
-                self.price = float(read[2])
-                total_list.append(self.price)
+                for read in spam_reader[1:]:
+                    self.price = float(read[2])
+                    total_list.append(self.price)
 
-                average_price = sum(total_list) / len(total_list)
-                volatility = ((max(total_list)) - (min(total_list)) / average_price) * 100
+                maximum = max(total_list)
+                minimum = min(total_list)
+                average = sum(total_list) / len(total_list)
+                volatility = round((maximum - minimum) / average * 100, 4)
 
-                self.result[self.name_ticker[7:11]] = round(volatility, 4)
+                if not volatility:
+                    null_result[ticker_name] = volatility
+                else:
+                    result[ticker_name] = volatility
 
-                total_list.clear()
-        print(self.result)
+        result = sorted(result.items(), key=lambda row: row[1], reverse=True)
+
+        def _print_result(data):
+            for _ticker_name, _volatility in data:
+                print(f'\t{_ticker_name} - {_volatility}', '%')
+
+        print('\nМаксимальная волатильность:')
+        _print_result(result[:3])
+
+        print('\nМинимальная волатильность:')
+        _print_result(result[len(result)-3:])
+
+        print('\nНулевая волатильность:')
+        _print_result(null_result.items())
 
 
 if __name__ == '__main__':
+    thread = Ticker('trades')
+    thread2 = Ticker('trades')
+    thread.start()
+    thread.join()
+    thread2.start()
+    thread2.join()
 
-    dir_name = os.listdir('trades')
-    lock = threading.Lock
-    count = [Ticker(dir_name='trades', name_ticker=line_ticker, lock=lock) for line_ticker in dir_name]
-
-    for ticker in count:
-        ticker.start()
-
-    for ticker in count:
-       ticker.join()
-       print(ticker)
